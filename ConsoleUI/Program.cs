d2afee3816258bdb36a4459da71fee0a8ab3838a
Yaşar İsgəndərov <yasharisgenderov.0@gmail.com>
2021-02-07 16:27:19 +0400

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using Business.Concret;
+using DataAccess.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new IMemoryCarDal());
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.CarId);
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/IMemoryCarDal.cs
@@ -0,0 +1,55 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete
+{
+    public class IMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public IMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{CarId = 1,BrandId=1,ColorId=1,ModelYear=2001,DailyPrice=10000,Description="Qezasiz"},
+                new Car{CarId = 2,BrandId=1,ColorId=1,ModelYear=2001,DailyPrice=10000,Description="Qezasiz"},
+                new Car{CarId = 3,BrandId=1,ColorId=1,ModelYear=2001,DailyPrice=10000,Description="Qezasiz"},
+                new Car{CarId = 4,BrandId=1,ColorId=1,ModelYear=2001,DailyPrice=10000,Description="Qezasiz"},
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            var result = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            _cars.Remove(result);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int CarId)
+        {
+            return _cars.Where(c => c.BrandId == CarId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            var UpdatedCar = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            UpdatedCar.ColorId = car.ColorId;
+            UpdatedCar.BrandId = car.BrandId;
+            UpdatedCar.DailyPrice = car.DailyPrice;
+            UpdatedCar.Description = car.Description;
+            UpdatedCar.ModelYear = car.ModelYear;
+        }
+    }
+}
