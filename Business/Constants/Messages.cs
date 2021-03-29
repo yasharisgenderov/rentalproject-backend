5bb16b3146fdc9d1c689b7ebb477547ed3c3541f
Yaşar İsgəndərov <yasharisgenderov.0@gmail.com>
2021-03-30 02:26:59 +0400

    30 Mart

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -6,10 +6,18 @@ namespace Business.Constants
 {
     public static class Messages
     {
-        public static string ProductAdded = "Urun eklendi";
-        public static string ProductNameInvalid = "Urun ismi gecersiz";
-        public static string MaintenanceTime = "Sistem bakimda";
-        public static string ProductsListed = "Urunler listelendi";
-        public static string UnitPriceInvalid = "";
+        public static string BrandAdded = "Brend elave edildi";
+        public static string BrandDeleted = "Brend silindi";
+        public static string BrandUpdated = "Brend guncellendi";
+
+        public static string RentalAdded = "Rental elave edildi";
+        public static string RentalDeleted = "Rental silindi";
+        public static string RentalUpdated = "Rental guncellendi";
+
+        public static string ProductAdded = "Urun elave edildi";
+        public static string ProductNameInvalid = "Product adi gecersizdir";
+        public static string MaintenanceTime = "Sistem proflaktik baxisda";
+        public static string ProductsListed = "Product listelendi";
+        public static string UnitPriceInvalid = "Productin Pricei kecerli deyil";
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Business.Concret;
+using Business.Concrete;
 using DataAccess.Concrete;
 using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
 using System;
 
 namespace ConsoleUI
@@ -10,12 +12,27 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             ProductTest();
+            RentalManager rentalManager = new RentalManager(new EfRentalDal());
+            var result = rentalManager.Add(new Rental
+            {
+                RentDate = DateTime.Now,
+                ReturnDate = DateTime.Now
+            });
+
+            if (result.Success)
+            {
+                Console.WriteLine(result.Message);
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
         }
 
         private static void ProductTest()
         {
             CarManager carManager = new CarManager(new EfCarDal());
-            foreach (var car in carManager.GetProductDetails())
+            foreach (var car in carManager.GetProductDetails().Data)
             {
                 Console.WriteLine(car.CarName + "/" + car.ColorName);
             }
