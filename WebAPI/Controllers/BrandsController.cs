72e6b0f77c5e1cfdff8671e2ffd40ae704cf4834
Yaşar İsgəndərov <yasharisgenderov.0@gmail.com>
2021-05-15 17:12:52 +0400

    15 May

--- WebAPI/Controllers/BrandsController.cs
+++ WebAPI/Controllers/BrandsController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WebAPI.Controllers
@@ -59,6 +60,7 @@ namespace WebAPI.Controllers
         [HttpGet("GetBrandsAll")]
         public IActionResult GetBrandsAll()
         {
+            Thread.Sleep(5000);
             var result = _brandService.GetAll();
             if (result.Success)
             {
